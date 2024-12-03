94e244d5636b1293f67377f13514b818786cab1f
Ayberk Tosun <ayberk.tosun@gmail.com>
2024-12-03 11:45:24 +0000

    Add solution for Day 3 Part 1

---
+++ day-3/Part1.hs
@@ -0,0 +1,41 @@
+module Part1 where
+
+import Data.List
+import Data.String
+import System.IO
+import Text.Regex.TDFA
+import Data.Char (isSpace)
+
+mulRegex :: String
+mulRegex = "mul\\([0-9]+,[0-9]+\\)"
+
+getMuls :: String -> [String]
+getMuls s = getAllTextMatches (s =~ mulRegex)
+
+split :: String -> (String, String)
+split s = (s1, s2)
+  where
+    s1 = takeWhile (/= ',') s
+    s2 = tail $ dropWhile (/= ',') s
+
+calculate :: String -> Int
+calculate ('m':'u':'l':s) =
+  let
+    (s1, s2) = split . init . tail $ s
+    (n1, n2) = (read s1, read s2) :: (Int, Int)
+  in
+    n1 * n2
+
+solution :: String -> Int
+solution ls = sum $ calculate <$> mulss
+  where
+    mulss :: [String]
+    mulss = getMuls ls
+
+main :: IO ()
+main = do
+  handle  <- openFile "input.txt" ReadMode
+  content <- hGetContents handle
+  let result = solution $ content
+  print result
+  hClose handle
