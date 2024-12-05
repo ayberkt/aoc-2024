000255cc899f3a74ab30cc576203e4de6276828c
Ayberk Tosun <ayberk.tosun@gmail.com>
2024-12-05 21:26:39 +0000

    Solve both parts of Day 5

--- day-5/Part1.hs
+++ day-5/Part1.hs
@@ -6,13 +6,40 @@ import System.IO
 import Data.Char (isSpace)
 import Data.Maybe
 
-solution :: [String] -> Int
-solution ss = undefined
+parseRule :: String -> (Int, Int)
+parseRule s = (read $ takeWhile (/= '|') s, read $ tail $ dropWhile (/= '|') s)
+
+parsePageList :: String -> [Int]
+parsePageList s = read $ "[" ++ s ++ "]"
+
+isSorted :: (a -> a -> Bool) -> [a] -> Bool
+isSorted ord []         = True
+isSorted ord [x]        = True
+isSorted ord (x1:x2:xs) = ord x1 x2 && isSorted ord (x2:xs)
+
+middle :: [a] -> a
+middle xs = xs !! ((length xs) `div` 2)
+
+solution :: [String] -> [String] -> Int
+solution s t = sum $ middle <$> filter (isSorted below) pageLists
+    where
+      rules :: [(Int, Int)]
+      rules = parseRule <$> s
+
+      below :: Int -> Int -> Bool
+      below m n = (m, n) `elem` rules
+
+      allNumbers :: [Int]
+      allNumbers = (fst <$> rules) ++ (snd <$> rules)
+
+      pageLists :: [[Int]]
+      pageLists = parsePageList <$> t
 
 main :: IO ()
 main = do
   handle  <- openFile "input.txt" ReadMode
   content <- hGetContents handle
-  let result = solution $ lines content
-  print result
+  let rules = takeWhile (/= "") $ lines content
+  let pages = tail . dropWhile (/= "") $ lines content
+  print $ solution rules pages
   hClose handle
--- day-5/Part2.hs
+++ day-5/Part2.hs
@@ -1,20 +1,33 @@
 module Part2 where
 
+import Data.Char (isSpace)
 import Data.List
+import Data.Maybe
 import Data.String
 import System.IO
-import Data.Char (isSpace)
-import Data.Maybe
 
 import qualified Part1
 
-solution :: [String] -> Int
-solution ss = undefined
+solution :: [String] -> [String] -> Int
+solution s t = sum $ Part1.middle <$> sortBy compare <$> pageLists
+  where
+   rules :: [(Int, Int)]
+   rules = Part1.parseRule <$> s
+
+   below :: Int -> Int -> Bool
+   below m n = (m, n) `elem` rules
+
+   compare :: Int -> Int -> Ordering
+   compare m n = if m `below` n then LT else GT
+
+   pageLists :: [[Int]]
+   pageLists = filter (not . Part1.isSorted below) $ Part1.parsePageList <$> t
 
 main :: IO ()
 main = do
   handle  <- openFile "input.txt" ReadMode
   content <- hGetContents handle
-  let result = solution $ lines content
-  print result
+  let rules = takeWhile (/= "") $ lines content
+  let pages = tail . dropWhile (/= "") $ lines content
+  print $ solution rules pages
   hClose handle
