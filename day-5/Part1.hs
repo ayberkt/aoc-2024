2144ada7f5b464a6ac8137eef19e96e84b601e5d
Ayberk Tosun <ayberk.tosun@gmail.com>
2024-12-05 21:28:51 +0000

    Clean up

--- day-5/Part1.hs
+++ day-5/Part1.hs
@@ -1,10 +1,10 @@
 module Part1 where
 
+import Data.Char (isSpace)
 import Data.List
+import Data.Maybe
 import Data.String
 import System.IO
-import Data.Char (isSpace)
-import Data.Maybe
 
 parseRule :: String -> (Int, Int)
 parseRule s = (read $ takeWhile (/= '|') s, read $ tail $ dropWhile (/= '|') s)
@@ -18,22 +18,22 @@ isSorted ord [x]        = True
 isSorted ord (x1:x2:xs) = ord x1 x2 && isSorted ord (x2:xs)
 
 middle :: [a] -> a
-middle xs = xs !! ((length xs) `div` 2)
+middle xs = xs !! (length xs `div` 2)
 
 solution :: [String] -> [String] -> Int
 solution s t = sum $ middle <$> filter (isSorted below) pageLists
-    where
-      rules :: [(Int, Int)]
-      rules = parseRule <$> s
+  where
+    rules :: [(Int, Int)]
+    rules = parseRule <$> s
 
-      below :: Int -> Int -> Bool
-      below m n = (m, n) `elem` rules
+    below :: Int -> Int -> Bool
+    below m n = (m, n) `elem` rules
 
-      allNumbers :: [Int]
-      allNumbers = (fst <$> rules) ++ (snd <$> rules)
+    allNumbers :: [Int]
+    allNumbers = (fst <$> rules) ++ (snd <$> rules)
 
-      pageLists :: [[Int]]
-      pageLists = parsePageList <$> t
+    pageLists :: [[Int]]
+    pageLists = parsePageList <$> t
 
 main :: IO ()
 main = do
