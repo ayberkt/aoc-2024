4a1bd00159e37278b26e45f57727bbf9df1d123b
Ayberk Tosun <ayberk.tosun@gmail.com>
2024-12-08 15:12:43 +0000

    Solve Day 8 Part 2

--- day-8/Part2.hs
+++ day-8/Part2.hs
@@ -7,13 +7,57 @@ import Data.Char
 
 import Utils
 
-solution :: [String] -> Int
-solution ls = undefined
+antinodesWRT :: Int -> Int -> (Int, Int) -> (Int, Int) -> [(Int, Int)]
+antinodesWRT ht wd v u = beta <$> range
+  where
+    withinGrid :: (Int, Int) -> Bool
+    withinGrid (i, j) = i >= 0 && i < ht && j >= 0 && j < wd
+
+    beta :: Int -> (Int, Int)
+    beta k = v `csub` (scale k $ v `csub` u)
+
+    range :: [Int]
+    range = takeWhile (withinGrid . beta) [1..]
+
+type Antenna = (Char, (Int, Int))
+
+allAntennaeinRow :: Int -> [Char] -> [Antenna]
+allAntennaeinRow i row = [ (c, (i, j)) | (c, j) <- zip row [0..l-1], c /= '.' ]
+  where
+    l = length row
+
+allAntennae :: [[Char]] -> [Antenna]
+allAntennae grid =
+  concat [ allAntennaeinRow i row | (row, i) <- zip grid [0..l-1]]
+    where
+      l = length grid
+
+groupAntennae :: [Antenna] -> [[Antenna]]
+groupAntennae = quotient fst
+
+numAntinodes :: Int -> Int -> [Antenna] -> [(Int, Int)]
+numAntinodes ht wd as = possibleAntinodes
+  where
+    vs :: [(Int, Int)]
+    vs = snd <$> as
+
+    possibleAntinodes :: [(Int, Int)]
+    possibleAntinodes = concat [ concat $ antinodesWRT ht wd v <$> (filter (/= v) vs) | v <- vs ]
 
 main :: IO ()
 main = do
-  handle  <- openFile "day-8/test.txt" ReadMode
+  handle  <- openFile "day-8/input.txt" ReadMode
   content <- hGetContents handle
-  let result = solution $ lines content
-  print result
+
+  let
+    ls = lines content
+    h  = length ls
+    w  = length $ head ls
+
+  let antennaeGrouped = groupAntennae (allAntennae ls) :: [[Antenna]]
+
+  print . nub . concat $ numAntinodes h w <$> antennaeGrouped
+  print . length . nub . concat $ numAntinodes h w <$> antennaeGrouped
+
+  -- print result
   hClose handle
